dc8a91a71ce480c5a26a04ade72040e59d9cbb13
tzuriel <tzurielluzzon@gmail.com>
2024-01-16 22:28:14 +0200

    add function

---
+++ src/inputHandler.cpp
@@ -0,0 +1,6 @@
+#include "inputHandler.h"
+
+bool inputHandler::checkURL(string url)
+{
+    return false;
+}
---
+++ src/inputHandler.h
@@ -0,0 +1,16 @@
+#ifndef FOOBAR_TD_INPUTHANDLER_H
+#define FOOBAR_TD_INPUTHANDLER_H
+#include <iostream>
+#include <string>
+
+using std::string;
+
+class inputHandler
+{
+public:
+    bool checkURL(string url);
+
+};
+
+
+#endif //FOOBAR_TD_INPUTHANDLER_H
